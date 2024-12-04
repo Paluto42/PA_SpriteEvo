941d1147e1b65f2d28221a3fafb55751a289d87e
Paluto <85861331+Paluto42@users.noreply.github.com>
2024-12-04 23:01:03 +0800

    fix

---
+++ Source/PA_SpriteEvo/SPE_ModSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace SpriteEvo
+{
+    public class SPE_ModConfig : Mod
+    {
+        public SPE_ModConfig(ModContentPack content) : base(content)
+        {
+
+        }
+        public override string SettingsCategory()
+        {
+            return "SpriteEvo";
+        }
+    }
+    public class SPE_ModSettings : ModSettings
+    {
+        public static bool debugOverride = false;
+    }
+}
