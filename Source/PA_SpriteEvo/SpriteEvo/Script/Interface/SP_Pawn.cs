db604bb71b9d38510c6938ffeba370eaf9175e84
Paluto <85861331+Paluto42@users.noreply.github.com>
2025-04-23 02:08:26 +0800

    Update SP_Pawn.cs

--- Source/PA_SpriteEvo/SpriteEvo/Script/Interface/SP_Pawn.cs
+++ Source/PA_SpriteEvo/SpriteEvo/Script/Interface/SP_Pawn.cs
@@ -55,7 +55,7 @@ namespace SpriteEvo
 
         int count = 0;
 
-        public float drawDepth = 0.5f;
+        public float drawDepth = 1;
         float blinkMixIn => Props.blinkMixIn;
         float blinkMixOut => Props.blinkMixOut;
 
